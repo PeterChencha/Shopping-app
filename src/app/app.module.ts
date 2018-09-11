35d506870f56cf2a823cb482f48232b16dea344a
extraordinaire1 <peter@chenchatech.com>
2018-09-11 16:39:46 +0300

    component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { UdemyComponent } from './udemy/udemy.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shoppingListFeature/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shoppingListFeature/shopping-list-edit/shopping-list-edit.component';
+import { recipeBookFeatureComponent } from './recipeBookFeature/recipeBookFeature.component'
 import { RecipeListComponent } from './recipeBookFeature/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipeBookFeature/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipeBookFeature/recipe-detail/recipe-detail.component';
@@ -23,6 +24,7 @@ import { RecipeDetailComponent } from './recipeBookFeature/recipe-detail/recipe-
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
+    recipeBookFeatureComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent
---
+++ src/app/recipeBookFeature/recipeBookFeature.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipeBookFeature',
+  templateUrl: './recipeBookFeature.component.html',
+  styleUrls: ['./recipeBookFeature.component.css']
+})
+
+export class recipeBookFeatureComponent{
+
+}

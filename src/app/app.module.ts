982043fbf1c8a29fce3fb077a0039f69ad7ca3a8
extraordinaire1 <peter@chenchatech.com>
2018-09-11 16:11:54 +0300

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,25 @@ import { AppComponent } from './app.component';
 import { serverComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { UdemyComponent } from './udemy/udemy.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shoppingListFeature/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shoppingListFeature/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipeBookFeature/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipeBookFeature/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipeBookFeature/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     serverComponent,
     ServersComponent,
-    UdemyComponent
+    UdemyComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,

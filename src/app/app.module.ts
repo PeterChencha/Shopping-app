a8a3ce0238e879f3fe0918a2acfcb9af1a6225e5
extraordinaire1 <peter@chenchatech.com>
2018-09-11 18:22:03 +0300

    component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,10 @@ import { ServersComponent } from './servers/servers.component';
 import { UdemyComponent } from './udemy/udemy.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shoppingListFeature/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shoppingListFeature/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListEditComponent } from './shoppingListFeature/shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { recipeBookFeatureComponent } from './recipeBookFeature/recipeBookFeature.component'
 import { RecipeListComponent } from './recipeBookFeature/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipeBookFeature/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './recipeBookFeature/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipeBookFeature/recipe-detail/recipe-detail.component';
 
 @NgModule({
